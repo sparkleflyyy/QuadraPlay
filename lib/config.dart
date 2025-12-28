f0d7b7d0cc9f2f290b51884014de5371a9b9ea33
Riyann <riyannurizqy@gmail.com>
2025-12-29 00:31:53 +0700

    up feautre drivers base

--- lib/config.dart
+++
@@ -1,5 +0,0 @@
-const String token = '690de4f3fcee2015d33ec864';
-const String project = 'sewa_ps';
-const String appid = '693accaf23173f13b93c1fed';
-const String fileurl = 'https://api.247go.app/v5/';
-const bool useUploadProxy = true;
